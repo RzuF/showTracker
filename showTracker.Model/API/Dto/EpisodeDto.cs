ff5ab02d10667de7c55c0195b5f8d8f7169f9799
MORPHEUS <MORPHEUS@DESKTOP-IJM2R8N>
2018-04-03 17:14:19 +0200

    #9 - Commit just to make new branch with proper name

---
+++ showTracker.Model/API/Dto/EpisodeDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace showTracker.Model.API.Dto
+{
+    public class EpisodeDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public int Season { get; set; }
+        public int Number { get; set; }
+        public DateTime AirDate { get; set; }
+        public DateTime AirTime { get; set; }
+        public DateTime AirStamp { get; set; }
+        public int Runtime { get; set; }
+        public ImageDto Image { get; set; }
+        public string Summary { get; set; }
+        [JsonProperty("_links")]
+        public LinksDto Links { get; set; }
+    }
+}
---
+++ showTracker.Model/API/Dto/ImageDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace showTracker.Model.API.Dto
+{
+    public class ImageDto
+    {
+    }
+}
