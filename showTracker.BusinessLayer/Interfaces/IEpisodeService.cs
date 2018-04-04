835234ca9406baded1cd33df55f4858f2f0fbf9a
MORPHEUS <MORPHEUS@DESKTOP-IJM2R8N>
2018-04-04 17:45:37 +0200

    #9 - #10 actually; added EpisodeService; needs testing

---
+++ showTracker.BusinessLayer/Interfaces/IEpisodeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showTracker.BusinessLayer.Interfaces
+{
+    public interface IEpisodeService
+    {
+        Task<string> GetEpisodes(int showId, bool includeSpecials);
+        Task<string> GetEpisodesByNumber(int showId, int seasonId, int episodeId);
+        Task<string> GetEpisodesByDate(int showId, DateTime date);
+
+    }
+}
---
+++ showTracker.BusinessLayer/Services/EpisodeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using showTracker.BusinessLayer.Interfaces;
+using showTracker.Model;
+
+namespace showTracker.BusinessLayer.Services
+{
+    public class EpisodeService : IEpisodeService
+    {
+        private readonly IHttpClientWrapper _httpClientWrapper;
+
+        public EpisodeService(IHttpClientWrapper httpClientWrapper)
+        {
+            _httpClientWrapper = httpClientWrapper;
+        }
+
+        public async Task<string> GetEpisodes(int showId, bool includeSpecials = true)
+        {
+            var specials = (includeSpecials == true) ? 1 : 0;
+            var response = await _httpClientWrapper.HttpClient.GetStringAsync($"{Constants.ApiUrl}shows/{showId}/episodes?specials={specials}");
+
+            return response;
+        }
+
+        public async Task<string> GetEpisodesByDate(int showId, DateTime date)
+        {
+            var iso8601Date = date.ToString("yyyy-MM-dd");
+            var response = await _httpClientWrapper.HttpClient.GetStringAsync($"{Constants.ApiUrl}shows/{showId}/episodesbydate?date={iso8601Date}");
+
+            return response;
+        }
+
+        public async Task<string> GetEpisodeByNumber(int showId, int seasonId, int episodeId)
+        {
+            var response = await _httpClientWrapper.HttpClient.GetStringAsync($"{Constants.ApiUrl}shows/{showId}/episodebynumber?season={seasonId}&number={episodeId}");
+
+            return response;
+        }
+    }
+}
