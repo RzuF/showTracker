02bcc9b04167b5f653c644d84b35ddc328d2ee1b
Tomasz Błahut <rzuf22@gmail.com>
2018-04-20 16:08:24 +0200

    #41 - added release logger and case of using it

---
+++ showTracker.BusinessLayer/Loggers/ReleaseLogger.cs
@@ -0,0 +1,19 @@
+﻿using showTracker.BusinessLayer.Interfaces;
+
+namespace showTracker.BusinessLayer.Loggers
+{
+    public class ReleaseLogger : ISTLogger
+    {
+        public void Log(object obj, bool doNotBreakLine = false)
+        {
+        }
+
+        public void Log(string message, bool doNotBreakLine = false)
+        {
+        }
+
+        public void LogWithSerialization(object obj, bool doNotBreakLine = false)
+        {
+        }
+    }
+}
