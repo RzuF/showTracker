4cbfe1201b2cd66ed440e3c483763e6bbf6a8317
Tomasz Błahut <rzuf22@gmail.com>
2018-06-07 21:39:55 +0200

    #47 - added trimming any leading or trailing white-space characters

--- showTracker/showTracker.View/FavouritiesPage/FavouritiesViewModel.cs
+++ showTracker/showTracker.View/FavouritiesPage/FavouritiesViewModel.cs
@@ -130,7 +130,7 @@ namespace showTracker.ViewModel.FavouritiesPage
 
             if (Filters.Genre != "")
             {
-                FilteredShows = FilteredShows.Where(x => x.Genres.Select(y => y.ToLower()).Contains(Filters.Genre.ToLower())).ToList();
+                FilteredShows = FilteredShows.Where(x => x.Genres.Select(y => y.ToLower()).Contains(Filters.Genre.Trim().ToLower())).ToList();
             }
 
             if (Filters.Status != StatusEnum.None)
